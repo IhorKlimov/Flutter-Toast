879a4fafb54678f084c48843f14b0add0df35f29
Ihor Klimov <iklimov6@gmail.com>
2018-05-20 19:47:59 +0300

    Android version

--- android/src/main/kotlin/com/myhexaville/fluttertoast/FlutterToastPlugin.kt
+++ android/src/main/kotlin/com/myhexaville/fluttertoast/FlutterToastPlugin.kt
@@ -1,23 +1,25 @@
 package com.myhexaville.fluttertoast
 
+import android.widget.Toast
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class FlutterToastPlugin(): MethodCallHandler {
+class FlutterToastPlugin(val registrar: Registrar) : MethodCallHandler {
   companion object {
     @JvmStatic
     fun registerWith(registrar: Registrar): Unit {
       val channel = MethodChannel(registrar.messenger(), "flutter_toast")
-      channel.setMethodCallHandler(FlutterToastPlugin())
+      channel.setMethodCallHandler(FlutterToastPlugin(registrar))
     }
   }
 
   override fun onMethodCall(call: MethodCall, result: Result): Unit {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    if (call.method.equals("showToast")) {
+      val message: String = call.argument("message")
+      Toast.makeText(registrar.context(), message, Toast.LENGTH_LONG).show()
     } else {
       result.notImplemented()
     }
--- lib/flutter_toast.dart
+++ lib/flutter_toast.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 class FlutterToast {
   static const MethodChannel _channel = MethodChannel('flutter_toast');
 
-  static showToast(String messagee) {
-    _channel.invokeMethod("showToast", {"message": messagee});
+  static showToast(String message) {
+    _channel.invokeMethod("showToast", {"message": message});
   }
 }
